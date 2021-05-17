6516914eb6ecab86ee1117032f1d36ff3a4fad1c
Alexa Roskowski <aroskows@testium.cs.uoregon.edu>
2021-05-16 23:44:58 -0700

    calcs wrong

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -26,8 +26,45 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    dist = control_dist_km
+    #print("OPEN")
+    if dist > brevet_dist_km:
+        if dist < ((brevet_dist_km * 0.2) + brevet_dist_km):
+            dist = brevet_dist_km
+        else:
+            #break its too big
+            return -1
+    
+    #print(brevet_start_time)
+    data = {200: 34, 400: 32, 600: 30, 1000: 28, 1300: 26}
+    prev_keys = []
+    for key in data:
+        if  dist <= key:
+            if dist > 200:
+                h, m = 0, 0
+                brev = brevet_dist_km
+                for i in reversed(prev_keys):
+                    print(i, brev, dist)
+                    print(data[brev])
+                    th, tm = helper(dist-i, brev, data)
+                    dist = dist - (dist- i)
+                    print(th, tm)
+                    h += th
+                    m += tm
+                    brev = i
+                break
+            
+            else:
+                h, m = helper(dist, brevet_dist_km, data)
+                #print("h =", h)
+                #print("m =", m)
+                break
+            prev_keys.appened(key)
 
+    rslt =  brevet_start_time.shift(hours = h, minutes = m)
+    #print(rslt)
+    return rslt
+#open_time(60, 600, arrow.now())
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -41,4 +78,74 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    data = {60: 20, 200: 15, 400: 15, 600: 15, 1000: 11.428, 1300: 13.333}
+    dist = control_dist_km
+    if dist > brevet_dist_km:
+        if dist <= ((brevet_dist_km * 0.2) + brevet_dist_km):
+            dist = brevet_dist_km
+        else:
+            print("too big")
+            #break its too big
+            return -1
+
+    if dist == brevet_dist_km:
+        print("EQUAL")
+        if dist == 200:
+            h = 13
+            m = 30
+        elif dist == 400:
+            h = 27
+            m = 0
+        elif dist == 1000:
+            h = 75
+            m = 0
+        elif dist == 600:
+            h = 40
+            m = 0
+
+    else:
+        prev_keys= []
+        for key in data:
+            if dist  <= key:
+                if dist > 200:
+                    h, m = 0, 0
+                    brev = brevet_dist_km
+                    for i in reversed(prev_keys):
+                        print(i, brev, dist)
+                        print(data[brev])
+                        th, tm = helper(dist-i, brev, data)
+                        dist = dist - (dist- i)
+                        print(th, tm)
+                        h += th
+                        m += tm
+                        brev = i
+                    break
+                  
+                else:
+                    h, m  = helper(dist, brevet_dist_km, data)
+                    if key == 60:
+                        h, m  = helper(dist, 60, data)
+                        h += 1
+                    #print("h =", h)
+                    #print("m =", m)
+                    break
+            if key != 60:
+                prev_keys.append(key)
+
+            
+    rslt = brevet_start_time.shift(hours = h, minutes = m)
+    #print(rslt)
+    return rslt
+
+def helper(dist, brev, data):
+    unMathed = (dist)/(data[brev])
+    #print(data[brev])
+    #print(unMathed)
+    h = (dist) // (data[brev])
+    m = round((unMathed - h) * 60)
+    return (h, m)
+
+
+
+#close_time(60, 600, arrow.now())
+#print(temp)
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -51,14 +51,17 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    brevet_dist_km = request.args.get('brevet_dist_km', type=int)
+    start = request.args.get('begin_date')
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    open_time = acp_times.open_time(km, brevet_dist_km, arrow.get(str(start))).format('YYYY-MM-DDTHH:mm')
+
+    close_time = acp_times.close_time(km, brevet_dist_km, arrow.get(str(start))).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
